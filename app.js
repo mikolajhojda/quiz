8fcff7fd3137bf5f50f22d674b128137c25b1dda
Mikołaj Hojda <32071801+mikolajhojda@users.noreply.github.com>
2018-10-05 14:29:57 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#add-question');
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection(form.select.value).add({
+        question: form.question.value,
+        good: parseInt(form.good.value),
+        answer: [
+            form.a0.value,
+            form.a1.value,
+            form.a2.value,
+            form.a3.value
+        ]
+    });
+    form.question.value = '';
+    form.good.value = '';
+    form.a0.value = '';
+});
